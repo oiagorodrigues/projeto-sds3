9a1adcfd1e2421099c01e71442b64db753e42bb7
Iago Rodrigues <yagomello16@gmail.com>
2021-05-03 13:22:39 -0300

    feat: add basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div className="container">
-      <h1 className="text-primary">Hello World!</h1>
-    </div>
+    <>
+      <NavBar />
+      <main className="container content">
+        <h1 className="text-primary">Olá Mundo!</h1>
+        <DataTable />
+      </main>
+      <Footer />
+    </>
   );
 }
 
