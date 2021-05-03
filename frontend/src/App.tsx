4a27417824c5b7f439f5248baa9c1e37477703c8
Iago Rodrigues <yagomello16@gmail.com>
2021-05-03 12:57:07 -0300

    deps: add bootstrap | add basic styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="container">
+      <h1 className="text-primary">Hello World!</h1>
+    </div>
+  );
 }
 
 export default App;
