81bce79cc25258176c68bb76bfcff0f2d4a4014a
Iago Rodrigues <yagomello16@gmail.com>
2021-05-07 23:38:27 -0300

    feat: dataTable integration

--- frontend/src/types/Sale.ts
+++ frontend/src/types/Sale.ts
@@ -1,3 +1,13 @@
+import { Seller } from './Seller';
+export interface Sale {
+  id: number
+  visited: number
+  deals: number
+  amount: number
+  date: string
+  seller: Seller
+}
+
 export interface SaleSum {
   sellerName: string,
   salesSum: number
@@ -7,4 +17,16 @@ export interface SaleSuccess {
   sellerName: string
   visited: number
   deals: number
+}
+
+export interface SalePage {
+  content?: Sale[]
+  last: boolean
+  totalElements: number
+  totalPages: number
+  size?: number
+  number: number
+  first: boolean
+  numberOfElements?: number
+  empty?: boolean
 }
